7eaf058ed066ca89bc85f70f65d18c6b43f324c9
Elias Ramirez <eliasalejo01@gmail.com>
2024-03-27 08:11:17 -0400

    ft: Making tokengating work on supergroups also

--- functions/checkForUserProfileNFT.ts
+++ functions/checkForUserProfileNFT.ts
@@ -4,16 +4,29 @@ import { decryptData } from "../utils/decryptData";
 import { getUserFromDB } from "../utils/users/getUserFromDB";
 
 export async function checkForUserProfileNFT(ctx: Context) {
+  console.log("Got message")
   if (ctx.from?.is_bot) return;
 
-  if (ctx.message?.chat.type === "group") {
+  const isGroup = ctx.message?.chat.type === "group";
+  const isSupergroup = ctx.message?.chat.type === "supergroup";
+
+  console.log("Chat type: ", ctx.message?.chat.type);
+  if (isGroup || isSupergroup) {
+    console.log("Checking...")
     if (ctx.message!.new_chat_members) {
+      console.log("Checking 2...")
       for (const member of ctx.message!.new_chat_members) {
         if (member.is_bot) continue;
 
         const userInfo = await getUserFromDB(member.id);
 
+        console.log("User info: ", userInfo);
+
         if (!userInfo) {
+          if (isSupergroup) {
+            ctx.unbanChatMember(member.id);
+            continue;
+          }
           ctx.banChatMember(member.id);
           continue;
         }
@@ -28,8 +41,14 @@ export async function checkForUserProfileNFT(ctx: Context) {
 
         const userWalletInfo = await connectivity.getUserInfo();
 
+        console.log("Wallet info: ", userWalletInfo)
+
         if (userWalletInfo.profiles.length === 0) {
           console.log("REMOVING: ", member);
+          if (isSupergroup) {
+            ctx.unbanChatMember(member.id);
+            continue;
+          }
           ctx.banChatMember(member.id);
           continue;
         }
